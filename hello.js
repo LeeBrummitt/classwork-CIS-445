8a7914b790e8b6c2aad86fc78f7d94f48691874e
Lee Brummitt <leebrummitt@gmail.com>
2018-09-08 07:11:27 +0000

    added second schedule

--- hello.js
+++ hello.js
@@ -9,6 +9,11 @@ http.createServer(function(request,response){
     +"<p>CIS 445  Mon Wed @9:30-10:45 am</p>"
     +"<p>CIS 245  Mon Wed @1:30-2:45 pm</p>"
     +"<p>MA 345   Tue Thr @8:00-9:15 am</p>"
+    +"<p> </p>"
+    +"<p>Mon: CIS 445 (9:30 - 10:45am),  CIS 245 (1:30 - 2:45 pm) </p>"
+    +"<p>Tue: MA 345 (8:00 - 9:15 am) </p>"
+    +"<p>Wed: CIS 445 (9:30 - 10:45am),  CIS 245 (1:30 - 2:45 pm) </p>"
+    +"<p>Thr: MA 345 (8:00 - 9:15 am)</p>"
     +"</body>"
     +"</HTML>";
     response.write(html);
